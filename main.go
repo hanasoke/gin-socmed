8dbe31ca21052ebc4eaa277c16b1c6cb4e2103eb
hanasbp <hanasoke@gmail.com>
2026-01-12 17:05:27 +0700

    update main.go & error_handler.go

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"gin-socmed/config"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
+	config.LoadConfig()
+	config.LoadDB()
+
+	r := gin.Default()
+	api := r.Group("/api")
+
+	api.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	router.Run() // listens on 0.0.0.0:8080 by default
+	r.Run(fmt.Sprintf(":%v", config.ENV.PORT))
 }
