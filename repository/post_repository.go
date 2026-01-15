7fa2fa66f1c5245a6cb9afaea1cc01407230ea9a
hanasbp <hanasoke@gmail.com>
2026-01-15 09:14:09 +0700

    updated post_repository.go file & added post_service.go file

--- repository/post_repository.go
+++ repository/post_repository.go
@@ -1,4 +1,16 @@
 package repository
 
+import "gorm.io/gorm"
+
 type PostRepository interface {
+}
+
+type postRepository struct {
+	db *gorm.DB
+}
+
+func NewPostRepository(db *gorm.DB) *postRepository {
+	return &postRepository{
+		db: db,
+	}
 }
